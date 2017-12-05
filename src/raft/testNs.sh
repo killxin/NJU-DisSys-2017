f67dc6666904bba248ed4d054f016f1571d549ce
rhjiang <1839843549@qq.com>
2017-12-05 17:23:32 +0800

    test N times

--- src/raft/testNs.sh
+++ src/raft/testNs.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-#testers=("TestBasicAgree" "TestFailAgree" "TestFailNoAgree" "TestConcurrentStarts" "TestRejoin" "TestBackup" "TestCount" "Persist" "TestFigure8" "TestUnreliableAgree" "TestFigure8Unreliable" "TestReliableChurn" "TestUnreliableChurn")
-testers=("Agree" "TestConcurrentStarts" "TestRejoin" "TestBackup" "TestCount" "Persist" "Figure8" "Churn")
-tester=${testers[0]}
+testers=("TestConcurrentStarts" "TestRejoin" "TestBackup" "TestCount" "Persist" "Figure8" "Churn" "Agree" "Election")
+for n in `seq ${#testers[*]}`
+do
+tester=${testers[n-1]}
+echo $tester
 for i in `seq 10`
 do
 go test -run $tester > /dev/null
 if [[ $? == 0 ]]
 then
-    echo "$i th $tester Passed"
+    echo "$i th Passed"
 else
-    echo "$i th $tester Failed"
+    echo "$i th Failed"
     exit 1
 fi
+done
+echo "================"
 done

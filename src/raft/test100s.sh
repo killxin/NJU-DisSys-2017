0728f2b277bd4c239dfe6f87c2e0d38797bf54c0
rhjiang <1839843549@qq.com>
2017-12-05 16:57:06 +0800

    test 100 times

--- src/raft/test100s.sh
+++ src/raft/test100s.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-for i in `seq 100` 
+tester="TestBackup"
+#testers=("TestBasicAgree" "TestFailAgree" "TestFailNoAgree" "TestConcurrentStarts" "TestRejoin" "TestBackup" "TestCount" "Persist" "TestFigure8" "TestUnreliableAgree" "TestFigure8Unreliable" "TestReliableChurn" "TestUnreliableChurn")
+testers=("Agree" "TestConcurrentStarts" "TestRejoin" "TestBackup" "TestCount" "Persist" "Figure8" "Churn")
+for i in `seq 100`
 do
-echo $i"th test:"
-go test -run Election
-echo "===================="
+go test -run $tester > /dev/null
+if [[ $? == 0 ]]
+then
+    echo "$i th $tester Passed"
+else
+    echo "$i th $tester Failed"
+    exit 1
+fi
 done
